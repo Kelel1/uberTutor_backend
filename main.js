648b24fc8fd090ccec313a9a5f245ffc55650bc3
TNORTHERN <traynorthern@yahoo.com>
2020-05-17 12:01:22 -0500

    user profile and role now properly displayed, and roles are creatable

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const dotenv                = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const { setupDB, getDB }    = require('./db/connect');
-const express               = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const User                  = require('./models/User');
-const Rating                = require('./models/Rating');
-const Roles                 = require('./models/Roles');
-const UserProfile           = require('./models/UserProfile');
-const Category              = require('./models/Category');
-
+const { setupDB, getDB } = require("./db/connect");
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const User = require("./models/User");
+const Rating = require("./models/Rating");
+const Roles = require("./models/Roles");
+const UserProfile = require("./models/UserProfile");
+const Category = require("./models/Category");
 
 const typeDefs = gql`
-  type User {
-    id: ID!
-    role: Role!
-    userProfile: UserProfile!
-    email: String!
-    name: String!
-  }
   type UserProfile {
+    id: ID!
+    user: String
     location: String
     avatar: String
     ratings: [Rating]
     createdAt: String!
     updatedAt: String!
+  }
+  type User {
+    id: ID!
+    role: Role
+    profile: UserProfile
+    email: String!
+    name: String!
   }
   type Rating {
     id: ID!
@@ -44,6 +45,7 @@ const typeDefs = gql`
     updatedAt: String!
   }
   type Role {
+    id: ID!
     name: String!
     description: String!
     createdAt: String!
@@ -51,44 +53,79 @@ const typeDefs = gql`
   }
   type Query {
     users: [User]
+    roles: [Role]
   }
   type Mutation {
-    createUser(name: String, email: String, password: String, location: String, avatar: String): User
+    createUser(
+      name: String
+      email: String
+      password: String
+      location: String
+      avatar: String
+      role: String
+    ): User
+    createRole(name: String, description: String): Role
   }
 `;
-
 
 setupDB();
 
 const resolvers = {
   Query: {
-    users: () => { 
-     return User.find({})
-    }
+    users: async () => {
+      return await User.find({});
+    },
+    roles: () => Roles.find({}),
   },
   Mutation: {
-    createUser: async(parent, {name, email, password, location, avatar}) => {
-      role = "4jfoejfer98ge8g"
+    createUser: async (
+      parent,
+      { name, email, password, location, avatar, categories, role }
+    ) => {
       const user = await new User({
         name,
         email,
         password,
-        role
+        role,
       });
-      const selectedRole = Roles.find({ _id: user.role })
-      if (selectedRole === 'User') {
-        
-      }
-      const profile = new Profile({
-        user: user.id,
+      const selectedRole = Roles.findOne({ _id: role }, { name: 1, _id: 0 });
+      let profile = await new UserProfile({
+        user: user._id,
         location,
-        avatar
-      })
-      profile.save();
+        avatar,
+      });
+      // if (selectedRole.name === "student") {
+      //   await profile.save();
+      // }
+      // if (selectedRole.name === "tutor") {
+      //   profile = await new UserProfile({
+      //     user: user._id,
+      //     location,
+      //     avatar,
+      //     categories,
+      //   });
+      //   await profile.save();
+      // }
+      await profile.save();
       return user.save();
+    },
+    createRole: async (parent, { name, description }) => {
+      const role = await new Roles({
+        name,
+        description,
+      });
+      return role.save();
+    },
+  },
+  User: {
+    profile: async (parent) => {
+      return await UserProfile.findOne({ user: parent.id });
+    },
+    role: async (parent) => {
+      return await Roles.findById(parent.role)
     }
-  }
-}
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -98,10 +135,12 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({port: 4000}, () =>
+app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
-
-app.get("/test", async(req, res)=>{
-
+// 5ec157163c46a830b8aadef3
+app.get("/test", async (req, res) => {
+  res.json({
+    profiles: await UserProfile.findOne({ user: "5ec157163c46a830b8aadef3" }),
+  });
 });
--- models/UserProfile.js
+++ models/UserProfile.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const userProfileSchema = new Schema({
   user: { type: String, required: true },
+  categories: {type: String},
   ratings: { type: [String] },
   location: { type: String },
   avatar: { type: String },
