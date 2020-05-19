7e13ba875243b44bc5103abfd583d45f37414a2c
Ineb01 <benjamin.bachmayr@gmail.com>
2020-05-19 21:34:13 +0200

    implementing sorting on userByFilter and userLike

--- graphql/modules/users/resolvers.js
+++ graphql/modules/users/resolvers.js
@@ -3,7 +3,7 @@ const Category = require("../../../models/Category");
 const Rating = require("../../../models/Rating");
 const Roles = require("../../../models/Role");
 const UserProfile = require("../../../models/UserProfile");
-const { register, login, getUserByToken } = require("./helpers");
+const { register, login, getUserByToken, userCompareName, userProfileCompareCategoryMatches } = require("./helpers");
 
 module.exports = {
   Query: {
@@ -11,7 +11,9 @@ module.exports = {
       return await User.find({});
     },
     usersLike: async (parent, { name }) => {
-      return await User.find({ name: new RegExp(name, "i") });
+      let user_array = await User.find({ name: new RegExp(name, "i") });      
+      user_array.sort((a,b) => userCompareName(a,b,name));
+      return user_array;
     },
     userByEmail: async (parent, { email }) => {
       return await User.findOne({ email: email });
@@ -26,21 +28,40 @@ module.exports = {
       return login(user);
     },
     usersByFilter: async (parent, { categories, agemin, agemax, page, entriesPerPage}) => {
+      //setting default values:
+      if(!page)page=1;
+      if(!entriesPerPage)entriesPerPage=10;
+
+      //compiling the findObj for the profile
       let findObj = {};
       if(categories)findObj.categories={$in:categories};
       if(agemin)findObj.age={$gte:agemin}
       if(agemax)findObj.age={...findObj.age, $lte:agemax}
 
+      //applying findObj to mongodb
+      console.log(findObj);
       let allProfiles = await UserProfile.find(findObj);
-      let allUserIDs = [];
 
-      allProfiles.forEach(profile => {
-        allUserIDs = [...allUserIDs, profile.user]
-      });
+      console.log(allProfiles);
 
-      if(!page)page=1;
-      if(!entriesPerPage)entriesPerPage=10;
-      return await User.find({_id:{$in: allUserIDs}}).skip((page-1)*entriesPerPage).limit(entriesPerPage);
+      //sorting
+      if(categories){
+        allProfiles.sort((a,b)=>userProfileCompareCategoryMatches(a,b,categories))
+      }
+
+
+      //getting usersIDs from profile
+      let allUsers = [];
+      console.log(allProfiles.length);
+      for (const profile of allProfiles) {
+        let curUser = await User.findOne({_id:profile.user});
+        console.log(curUser);
+        if(curUser)allUsers = [...allUsers, curUser];
+      }
+      console.log(allUsers);
+      
+      
+      return allUsers//.slice((page-1)*entriesPerPage,(page-1)*entriesPerPage+entriesPerPage);
     },
   },
   Mutation: {
@@ -58,7 +79,6 @@ module.exports = {
   },
   UserProfile: {
     categories: async (parent) => {
-      console.log(parent.categories);
       return await Category.find({ _id: { $in: parent.categories } });
     },
     ratings: async (parent) => {
